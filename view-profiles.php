4f058e47a7dd40b2bb4fd3d2bce57b987c3c563c
Stallmanslave <123569961+adithyansreejith@users.noreply.github.com>
2024-08-04 12:22:00 +0530

    Update view-profiles.php
    refined more code

--- view-profiles.php
+++ view-profiles.php
@@ -421,61 +421,7 @@ if (isset($_GET['addToFavouriteId']) && $userObj["user_role"] === "premium") {
 
     </div>
 
-    <!-- Modal
-    <div class="modal fade" id="loginModal" tabindex="-1" role="dialog" aria-labelledby="loginModal" aria-hidden="true">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="loginModalLabel">Oops!!</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <div class="modal-body">
-                    <p>You are not logged in. To use this feature please login or create a profile.</p>
-                </div>
-                <div class="modal-footer">
-                    <a href="./login.php" class="btn btn-success">Login</a>
-                    <a href="./register.php" class="btn btn-primary">Register</a>
-                </div>
-            </div>
-        </div>
-    </div>
 
-    <div class="modal fade" id="addToFavouriteModal" tabindex="-1" role="dialog" aria-labelledby="addToFavouriteModal"
-         aria-hidden="true">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="addToFavouriteModalTitle">Oops!!</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <div class="modal-body">
-                    <div class="container-fluid">
-                        <div class="row">
-                            Hey! You must be a premium member to add users to your favourite list.
-                        </div>
-                        <hr>
-                        <div class="row">
-                            <strong>Perks of becoming premium members:</strong>
-                            <ul>
-                                <li>Have your own favourite list of users.</li>
-                                <li>See when a user sees and reads your message.</li>
-                                <li>Add/Remove users from favourite list</li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                    <a href="./become-premium-member.php" class="btn btn-primary"
-                       name="BecomePremium">Become a premium member</a>
-                </div>
-            </div>
-        </div>
-    </div>-->
 
     <script>
         $(document).ready(function () {
